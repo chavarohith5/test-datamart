63d8b74d66a6d1dfeaf354219832e3f4b685d726
tarunsharma1808 <tarunsharma1808@gmail.com>
2021-06-26 12:03:59 +0530

    change back ppk to pem

--- com/dsm/target_data_loading.py
+++ com/dsm/target_data_loading.py
@@ -36,13 +36,23 @@ if __name__ == '__main__':
 
         if tgt == 'REGIS_DIM':
             print('Creating REGIS_DIM table data')
-            cp_df = spark.read\
-                .parquet(staging_path + '/' + tgt_conf['source_data'])
-            cp_df.show()
-            cp_df.createOrReplaceTempView(tgt_conf['source_data'])
+            spark.read\
+                .parquet(staging_path + '/' + tgt_conf['source_data'])\
+                .createOrReplaceTempView(tgt_conf['source_data'])
 
-            spark.sql(tgt_conf['loadingQuery']).show()
+            regis_dim_df = spark.sql(tgt_conf['loadingQuery']).show()
+
+            jdbc_url = ut.get_redshift_jdbc_url(app_secret)
+
+            regis_dim_df.coalesce(1).write\
+                .format("io.github.spark_redshift_community.spark.redshift") \
+                .option("url", jdbc_url) \
+                .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
+                .option("forward_spark_s3_credentials", "true") \
+                .option("dbtable", tgt_conf['tableName']) \
+                .mode("overwrite")\
+                .save()
 
 
-# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" com/dsm/target_data_loading.py
+# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" com/dsm/target_data_loading.py
 
